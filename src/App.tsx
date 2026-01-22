8f47e7314ba7ead150dc135aea5156693ade80fe
Goncalo Taleigo <goncalotaleigo@gmail.com>
2026-01-22 12:52:34 +0000

    Added missing fonts, initial styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Task from './components/Task/Task'
 import './App.scss'
 
 function App() {
@@ -7,7 +7,21 @@ function App() {
 
   return (
     <>
-    
+      <div className='container-tasklist'>
+        <div className="container-tasklist__top">
+          <h1>TaskList</h1>
+          <button className='container-tasklist__button'>
+            <img src="../src/assets/icons/add.svg" alt="" />
+            Add task
+            </button>
+        </div>
+
+        <div className='container-tasklist__list'>
+          <Task/>
+        </div>
+
+      </div>
+
     </>
   )
 }
